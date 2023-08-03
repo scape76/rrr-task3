b9f1146afd3725fa419bb26579266b89ab6c4670
scape76 <danyaolekhq@gmail.com>
2023-08-03 13:34:51 +0300

    use port env variable

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -9,7 +10,10 @@ async function bootstrap() {
   });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get("PORT") || 3000);
 }
 
 bootstrap();
