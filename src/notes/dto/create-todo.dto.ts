1bc80538b526a5258eb83afb4416aa8a4d7999a3
scape76 <danyaolekhq@gmail.com>
2023-08-04 12:45:04 +0300

    add isArchived validation

--- src/notes/dto/create-todo.dto.ts
+++ src/notes/dto/create-todo.dto.ts
@@ -4,28 +4,37 @@ import {
   MaxLength,
   IsEnum,
   IsOptional,
+  IsNotEmpty,
+  IsBoolean,
 } from 'class-validator';
 import { Category } from 'src/types/notes';
 
 export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
   @MinLength(3, {
     message: 'name is too short',
   })
   @MaxLength(63, {
     message: 'name is too long',
   })
-  @IsString()
   name: string;
 
+  @IsString()
+  @IsNotEmpty()
   @MinLength(3, {
     message: 'content is too short',
   })
   @MaxLength(255, {
     message: 'content is too long',
   })
-  @IsString()
   content: string;
 
   @IsEnum(Category)
+  @IsNotEmpty()
   category: Category;
+
+  @IsBoolean()
+  @IsOptional()
+  isArchived: boolean;
 }
